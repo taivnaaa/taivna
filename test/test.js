15becba8ba3206e2f3f6ec4c2b45c9912f09c167
taivnaaa <taivnaaa182@gmail.com>
2023-11-28 19:07:43 -0800

    11-28

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
-console.log("Hi, developers!");
-alert("Hello world");
-let miniiNas = "55";
-let namme = "Taivanjargal";
+let number = 8;
 
-console.log(miniiNas);
-console.log(namme);
+if (number == 1) {
+  console.log("Davaa");
+} else if (number == 2) {
+  console.log("Myagmar");
+} else if (number == 3) {
+  console.log("Lkhagva");
+} else if (number == 4) {
+  console.log("Purev");
+} else if (number == 5) {
+  console.log("Baasan");
+} else if (number == 6) {
+  console.log("Bymba");
+} else if (number == 7) {
+  console.log("Nyam");
+} else if (number <= 0 || number >= 8) {
+  console.log("Tiim udru bahgui");
+}
