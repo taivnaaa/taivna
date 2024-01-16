14f87276337f496f74b03b5c1e016ea4ec5020ab
Тайванжаргал Лхагвасүрэн <23LP9793@G401-CC0101.local>
2024-01-15 19:45:32 -0800

    robo

--- react-taivna/src/components/Comp.jsx
+++ react-taivna/src/components/Comp.jsx
@@ -1,4 +1,47 @@
-export const List = (props) => {
-  const { first_name } = props;
-  return <div>{first_name}</div>;
+export const style = {
+  bodyy: {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+  },
+  constainer: {
+    padding: "15px",
+    width: "300px",
+    display: "flex",
+    flexDirection: "column",
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  },
+  img: {
+    width: "300px",
+  },
+  firstLast: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "10px",
+  },
+  genderEmp: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "15px",
+  },
+};
+
+export const Box = (props) => {
+  return (
+    <div style={style.constainer}>
+      <img style={style.img} src={props.img} alt="" />
+      <div style={style.firstLast}>
+        <p>{props.firstName}</p>
+        <p>{props.lastName}</p>
+      </div>
+      <div style={style.genderEmp}>
+        <p>{props.gender}</p>
+        <p>{props.employment}</p>
+      </div>
+      <div>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  );
 };
